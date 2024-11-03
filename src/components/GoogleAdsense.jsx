daa8568489c7a3686de5a23eae05a76c8ec30632
Avyakta000 <100035215+Avyakta000@users.noreply.github.com>
2024-11-03 14:48:29 +0530

    adsense

---
+++ src/components/GoogleAdsense.jsx
@@ -0,0 +1,19 @@
+// components/GoogleAdsense.js
+import Script from "next/script";
+
+const GoogleAdsense = ({ pId }) => {
+  // Only load AdSense in production
+  if (process.env.NODE_ENV !== "production") {
+    return null;
+  }
+  return (
+    <Script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
+  );
+};
+
+export default GoogleAdsense;
